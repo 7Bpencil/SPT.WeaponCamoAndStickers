dcf36d46b5f5a00a8d2d36c7d5fe826139470539
7Bpencil <Edward.Ekb@yandex.ru>
2026-03-14 07:03:57 +0500

    init

---
+++ Patches.cs
@@ -0,0 +1,27 @@
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using EFT;
+using EFT.Hideout;
+using EFT.Hideout.ShootingRange;
+using EFT.Interactive;
+using EFT.Interactive.SecretExfiltrations;
+using EFT.InventoryLogic;
+using EFT.Quests;
+using EFT.SynchronizableObjects;
+using EFT.UI;
+using EFT.UI.Screens;
+using EFT.Vehicle;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
+using SPT.Reflection.Patching;
+using JetBrains.Annotations;
+using HarmonyLib;
+using UnityEngine;
+
+namespace SevenBoldPencil.WeaponCamo
+{
+
+}
---
+++ Plugin.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using EFT;
+using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SevenBoldPencil.WeaponCamo
+{
+    [BepInPlugin("7Bpencil.WeaponCamo", "7Bpencil.WeaponCamo", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static Plugin Instance;
+		public ManualLogSource LoggerInstance;
+
+        public static ConfigEntry<KeyboardShortcut> SpawnButton;
+
+
+        private void Awake()
+        {
+            Instance = this;
+			LoggerInstance = Logger;
+
+            SpawnButton = Config.Bind("Main", "Spawn Button", new KeyboardShortcut(KeyCode.F4), "Spawn Button");
+        }
+
+		public void Update()
+		{
+			if (Input.GetKeyDown(SpawnButton.Value.MainKey))
+			{
+			}
+		}
+
+    }
+}
