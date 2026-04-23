a1f6f018ba128b09c62d332a5f2f6486c46c4342
7Bpencil <Edward.Ekb@yandex.ru>
2026-04-23 18:16:27 +0500

    bump version number

--- Client/Plugin.cs
+++ Client/Plugin.cs
@@ -134,7 +134,7 @@ namespace SevenBoldPencil.WeaponCamoAndStickers
         public HashSet<string> MasksDirectory;
     }
 
-    [BepInPlugin("7Bpencil.WeaponCamoAndStickers", "7Bpencil.WeaponCamoAndStickers", "1.4.0")]
+    [BepInPlugin("7Bpencil.WeaponCamoAndStickers", "7Bpencil.WeaponCamoAndStickers", "1.4.1")]
     public class Plugin : BaseUnityPlugin
     {
         public const string DefaultCamoName = "builtin/camos/default.png";
