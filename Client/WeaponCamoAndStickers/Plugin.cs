27ba4e4e7213a7ca8ce1bb37a13ca5e3bfa2e370
7Bpencil <Edward.Ekb@yandex.ru>
2026-05-16 13:37:44 +0500

    bump version number

--- Client/WeaponCamoAndStickers/Plugin.cs
+++ Client/WeaponCamoAndStickers/Plugin.cs
@@ -151,7 +151,7 @@ namespace SevenBoldPencil.WeaponCamoAndStickers
         public HashSet<string> MasksDirectory;
     }
 
-    [BepInPlugin("7Bpencil.WeaponCamoAndStickers", "7Bpencil.WeaponCamoAndStickers", "1.8.0")]
+    [BepInPlugin("7Bpencil.WeaponCamoAndStickers", "7Bpencil.WeaponCamoAndStickers", "1.9.0")]
     [BepInDependency("com.fika.core", BepInDependency.DependencyFlags.SoftDependency)]
     public class Plugin : BaseUnityPlugin
     {
