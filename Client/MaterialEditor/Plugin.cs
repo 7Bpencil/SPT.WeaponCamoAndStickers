b26e34e071390ba18eba60d26d3b5b1b30aa2a42
7Bpencil <Edward.Ekb@yandex.ru>
2026-05-22 03:29:54 +0500

    bump version number

--- Client/MaterialEditor/Plugin.cs
+++ Client/MaterialEditor/Plugin.cs
@@ -79,7 +79,7 @@ namespace SevenBoldPencil.MaterialEditor
         public MaterialInfo Material;
     }
 
-    [BepInPlugin("7Bpencil.MaterialEditor", "7Bpencil.MaterialEditor", "1.1.0")]
+    [BepInPlugin("7Bpencil.MaterialEditor", "7Bpencil.MaterialEditor", "1.2.0")]
     [BepInDependency("7Bpencil.WeaponCamoAndStickers", BepInDependency.DependencyFlags.HardDependency)]
     [BepInDependency("com.fika.core", BepInDependency.DependencyFlags.SoftDependency)]
     public class Plugin : BaseUnityPlugin
--- Client/WeaponCamoAndStickers/Plugin.cs
+++ Client/WeaponCamoAndStickers/Plugin.cs
@@ -151,7 +151,7 @@ namespace SevenBoldPencil.WeaponCamoAndStickers
         public HashSet<string> MasksDirectory;
     }
 
-    [BepInPlugin("7Bpencil.WeaponCamoAndStickers", "7Bpencil.WeaponCamoAndStickers", "1.10.0")]
+    [BepInPlugin("7Bpencil.WeaponCamoAndStickers", "7Bpencil.WeaponCamoAndStickers", "1.11.0")]
     [BepInDependency("com.fika.core", BepInDependency.DependencyFlags.SoftDependency)]
     public class Plugin : BaseUnityPlugin
     {
